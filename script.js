8bce8d83251bf19984e7c393187c8afa795f51d2
Omar Abotahoon <oabot040@uottawa.ca>
2023-06-02 22:14:24 -0400

    finished contact page and started search results page

---
+++ script.js
@@ -0,0 +1,7 @@
+//Homepage Search button
+const search_button = document.querySelector("#search_button");
+search_button.addEventListener("click", pageRedirect);
+
+function pageRedirect(){
+    window.location.href = "search_results.html";
+}
